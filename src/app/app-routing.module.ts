095c67e13b0c16ef3cdc660c8682f6ac2986b082
Felipe R Farias <feliperfariasdev@gmail.com>
2022-10-29 09:25:42 -0300

    estrutura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListarProdutosComponent } from './components/produtos/listar/listar-produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'listar-produtos', component: ListarProdutosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-component',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent {
+}
